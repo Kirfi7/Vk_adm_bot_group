895b308ff433537db79bf9219d8e8da746c6ba91
Pearson77 <mialal0505@gmail.com>
2023-02-10 20:01:36 +0300

    1.2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-# 1
+# 1213
 from pprint import pprint
 
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
