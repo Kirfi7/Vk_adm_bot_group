1bfa88b09ae5fccaa383a4a21bb664cf39d4d2a5
Pearson77 <mialal0505@gmail.com>
2023-02-11 11:20:32 +0300

    5.0

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import gspread
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from oauth2client.service_account import ServiceAccountCredentials
+
 # from pprint import pprint
 
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets",
@@ -13,6 +14,7 @@ creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 client = gspread.authorize(creds)
 sheet = client.open("ADMINS RED1").sheet1
 data = sheet.get_all_values()
+
 
 # cola = sheet.col_values(14)
 # col = sheet.col_values(2)
@@ -42,87 +44,115 @@ def sender(for_user_id, message_text):
     vk_session.method("messages.send", {"user_id": for_user_id, "message": message_text, "random_id": 0})
 
 
+def chat_sender(for_chat_id, message_text):
+    vk_session.method("messages.send", {"chat_id": for_chat_id, "message": message_text, "random_id": 0})
+
+
 def access(from_user_id):
-    ids_column = sheet.col_values(7); column_number = 0
+    ids_column = sheet.col_values(7)
+    column_number = 0
     for i in ids_column:
         column_number += 1
         if str(i) == str(from_user_id):
             return column_number, 1
     return 0, 0
 
-global ranked_up
 
 def get_default_info(array):
-    nick_name = array[1]
-    date = array[4]
-    admin_post = array[2]
-    alt_admin_post = array[3]
-    rank_up_date = array[11]
-    admin_level = array[12]
-    reports = array[17]
-    days_by_rank_up = array[18]
-    days_by_date = array[19]
-    punish_1 = array[14]
-    punish_2 = array[15]
-    punish_3 = array[16]
 
+    if array[12] == "1":
+        if int(array[19]) < 13 and int(array[17]) < 4000:
+            rank_up_message = f"Дней до повышения: {13 - int(array[19])}" \
+                              f"Ответов до повышения: {4000 - int(array[17])}"
 
+        elif int(array[19]) >= 13 and int(array[17]) < 4000:
+            rank_up_message = f"Ответов до повышения: {4000 - int(array[17])}"
 
-    if admin_level == str("1"):
-        ranked_up = "Допущен" if (int(reports) >= 4000 and int(days_by_rank_up) >= 13) else "Отсутствует"
+        elif int(array[19]) < 13 and int(array[17]) >= 4000:
+            rank_up_message = f"Дней до повышения: {13 - int(array[19])}"
 
-    elif admin_level == str("2"):
-        ranked_up = "Допущен" if (int(reports) >= 8000 and int(days_by_rank_up) >= 21) else "Отсутствует"
+        else:
+            rank_up_message = "К повышению допущен(-а)!"
 
-    elif admin_level == str("3"):
-        ranked_up = "Допущен" if (int(reports) >= 25000 and int(days_by_rank_up) >= 50) else "Отсутствует"
+    elif array[12] == "2":
+        if int(array[19]) < 21 and int(array[17]) < 8000:
+            rank_up_message = f"Дней до повышения: {21 - int(array[19])}" \
+                              f"Ответов до повышения: {8000 - int(array[17])}"
 
+        elif int(array[19]) >= 21 and int(array[17]) < 8000:
+            rank_up_message = f"Ответов до повышения: {8000 - int(array[17])}"
+
+        elif int(array[19]) < 21 and int(array[17]) >= 8000:
+            rank_up_message = f"Дней до повышения: {21 - int(array[19])}"
+
+        else:
+            rank_up_message = "К повышению допущен(-а)!"
+
+    elif array[12] == "3":
+        if int(array[19]) < 50 and int(array[17]) < 25000:
+            rank_up_message = f"Дней до повышения: {50 - int(array[19])}\n" \
+                              f"Ответов до повышения: {25000 - int(array[17])}"
+
+        elif int(array[19]) >= 50 and int(array[17]) < 25000:
+            rank_up_message = f"Ответов до повышения: {25000 - int(array[17])}"
+
+        elif int(array[19]) < 50 and int(array[17]) >= 25000:
+            rank_up_message = f"Дней до повышения: {50 - int(array[19])}"
+
+        else:
+            rank_up_message = "К повышению допущен(-а)!"
 
     else:
-        ranked_up = "Максимальный уровень"
+        rank_up_message = "Достигнут максимальный уровень!"
+
+    if (rank_up_message == "К повышению допущен(-а)!") and (int(array[14]) + int(array[15]) + int(array[16]) > 0):
+        rank_up_message = "Имеются активные наказания!"
 
     return f"🔑 Основная информация 🔑\n" \
-           f"Ваш никнейм: {nick_name}\n" \
-           f"Должность: {admin_post}\n" \
-           f"Доп. должность: {alt_admin_post}\n" \
-           f"Уровень админ-прав: {admin_level}\n" \
+           f"Ваш никнейм: {array[1]}\n" \
+           f"Должность: {array[2]}\n" \
+           f"Доп. должность: {array[3]}\n" \
+           f"Уровень админ-прав: {array[12]}\n" \
            f"\n📅 Важные даты и дни 📅\n" \
-           f"Дата постановки: {date}\n" \
-           f"Дата повышения: {rank_up_date}\n" \
-           f"Дней с повышения: {days_by_rank_up}\n" \
-           f"Дней на админ-посту: {days_by_date}\n" \
+           f"Дата постановки: {array[4]}\n" \
+           f"Последнее повышение: {array[11]}\n" \
+           f"Дней с момента повышения: {array[18]}\n" \
+           f"Всего дней на админ-посту: {array[19]}\n" \
            f"\n⛔️ Активные наказания ⛔️\n" \
-           f"Количество выговоров: {punish_1}\n" \
-           f"Количество предов: {punish_2}\n" \
-           f"Количество устных: {punish_3}\n\n" \
-           f"✅ Общее кол-во ответов: {reports}\n" \
-           f"Допуск к повышению: {ranked_up}\n"
+           f"Количество выговоров: {array[14]}\n" \
+           f"Количество предов: {array[15]}\n" \
+           f"Количество устных: {array[16]}\n\n" \
+           f"✅ Общее кол-во ответов: {array[17]}\n" \
+           f"\n🔑 Информация о повышении 🔑\n" \
+           f"{rank_up_message}"
 
-
-prefix = ["/", "!", "+"]
 
 vk_session = vk_api.VkApi(token="vk1.a.RcpYvhXvj2P9NOfFXQ_ycHiFm1ENdqSKt4iRTR4_YltoNejArEwUNnQqy--Hq3vvyG_3-smfpgjNT-rHuUpNVcdJfMBRvV7CXhw0eL-T6dBYW-nAI1t_9834yfJpQBQaO1Y2Is9lRjDePfVEGwObaCyNOEWXaZL_K4OC9a7XXgbrpgGm-6vMOxShAufDs46qU34iqIervEGqItexmEX8nA")
 lp = VkLongPoll(vk_session)
 vk = vk_session.get_api()
 
+while True:
+    try:
+        for event in lp.listen():
+            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                text = event.text
+                user_id = event.user_id
 
-for event in lp.listen():
-    if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-        text = event.text
-        user_id = event.user_id
+                if access(user_id)[1] == 1 and text == "INFO":
+                    line_id = access(user_id)[0]
+                    row = sheet.row_values(line_id)
+                    values_array = list(row)
+                    sender(user_id, get_default_info(values_array))
 
-        if access(user_id)[1] != 0 and text == "INFO":
-            line_id = access(user_id)[0]
-            row = sheet.row_values(line_id)
-            values_array = list(row)
-            sender(user_id, get_default_info(values_array))
+                elif event.text.lower() == "начать":
+                    keyboard = VkKeyboard()
+                    keyboard.add_button("INFO", VkKeyboardColor.POSITIVE)
+                    vk_session.method("messages.send", {
+                        "user_id": user_id,
+                        "message": "Вы авторизовались в боте!",
+                        "random_id": 0,
+                        "keyboard": keyboard.get_keyboard()
+                    })
 
-        elif event.text.lower() == "начать":
-            keyboard = VkKeyboard()
-            keyboard.add_button("INFO", VkKeyboardColor.POSITIVE)
-            vk_session.method("messages.send", {
-                "user_id": user_id,
-                "message": "Вы авторизовались в боте!",
-                "random_id": 0,
-                "keyboard": keyboard.get_keyboard()
-            })
+    except Exception as error:
+        chat_sender(1, error)
