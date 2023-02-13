b87231da66436734bd6dee6ebeba46589a1d2ba2
Pearson77 <mialal0505@gmail.com>
2023-02-13 17:46:44 +0300

    7.0

--- main.py
+++ main.py
@@ -70,37 +70,74 @@ def get_info_about_rank(array):
     admin_lvl = array[12]
 
     rank_standards = {
-        "1": {"days": 13, "reports": 4000},
-        "2": {"days": 21, "reports": 8000},
-        "3": {"days": 50, "reports": 25000}
+        "1": {"days": 13, "reports": 4000, "this_rank": "Младшего Модератора", "next_rank": "Модератора"},
+        "2": {"days": 21, "reports": 8000, "this_rank": "Модератора", "next_rank": "Администратора"},
+        "3": {"days": 50, "reports": 25000, "this_rank": "Администратора", "next_rank": "Старшего Администратора"}
     }
 
     if int(admin_lvl) < 4:
         reports = int(array[17]); up_date = int(array[18])
-        p1 = array[14]; p2 = array[15]; p3 = array[16]
+        punish = [int(array[14]), int(array[15]), int(array[16])]
+        t = f"С {rank_standards[admin_lvl]['this_rank']} на {rank_standards[admin_lvl]['next_rank']}:"
 
-        days_by_lvl = rank_standards[admin_lvl]['days']
-        reps_by_lvl = rank_standards[admin_lvl]['reports']
+        days_by_lvl = int(rank_standards[admin_lvl]['days'])
+        reps_by_lvl = int(rank_standards[admin_lvl]['reports'])
 
         if days_by_lvl > up_date and reps_by_lvl > reports:
-            return cfg.rank_up_message(admin_lvl, (reps_by_lvl - reports), (days_by_lvl - up_date), p1, p2, p3, 0)
+            return cfg.rank_up(reps_by_lvl, days_by_lvl, (reps_by_lvl - reports), (days_by_lvl - up_date), punish, 0, t)
 
         elif days_by_lvl > up_date and reps_by_lvl <= reports:
-            return cfg.rank_up_message(admin_lvl, 0, (days_by_lvl - up_date), p1, p2, p3, 0)
+            return cfg.rank_up(reps_by_lvl, days_by_lvl, 0, (days_by_lvl - up_date), punish, 0, t)
 
         elif days_by_lvl <= up_date and reps_by_lvl > reports:
-            return cfg.rank_up_message(admin_lvl, (reps_by_lvl - reports), 0, p1, p2, p3, 0)
+            return cfg.rank_up(reps_by_lvl, days_by_lvl, (reps_by_lvl - reports), 0, punish, 0, t)
 
         else:
-            return cfg.rank_up_message(admin_lvl, (reps_by_lvl - reports), 0, p1, p2, p3, 1)
+            return cfg.rank_up(reps_by_lvl, days_by_lvl, (reps_by_lvl - reports), 0, punish, 1, t)
 
     else:
         return "Достигнут максимальный админ-уровень!"
 
-    # if (rank_up_message == "К повышению допущен(-а)!") and (int(array[14]) + int(array[15]) + int(array[16]) > 0):
-    #     rank_up_message = "Имеются активные наказания!"
-    #
-    # default_massage = f"Ваш текущий уровень: {admin_lvl}\nДля повышения необходимо:\nОтветов — {} | Дней — {}"
+
+def get_rank_standard(get_admin):
+    post = get_admin[0]
+    second = get_admin[1]
+
+    rank_standards = {
+        "ММ": "Ответы: 250 штук\nОнлайн: 150 минут\nНаказаний: 10 штук",
+        "М": "Ответы: 200 штук\nОнлайн: 150 минут\nНаказаний: 10 штук\nМероприятия: 2 штуки",
+        "МК": "Ответы: 175 штук\nОнлайн: 150 минут\nНаказаний: 15 штук\nПост: За своей фракцией",
+        "МФ": "Ответы: 150 штук\nОнлайн: 120 минут\nНаказаний: 10 штук\nФорум: 10 ответов\nМероприятия: 1 штука",
+        "МК&МФ": "Ответы: 170 штук\nОнлайн: 150 минут\nНаказаний: 15 штук\nПост: За своей фракцией\nФорум: 10 ответов\nМероприятия: 1 штука",
+        "А": "Ответы: 200 штук\nОнлайн: 150 минут\nНаказаний: 15 штук\nМероприятия: 2 штуки",
+        "СА": "Ответы: 200 штук\nОнлайн: 150 минут\nНаказаний: 20 штук\nМероприятия: 1 штука",
+        "ЗГС": "Ответы: 120 штук\nОнлайн: 130 минут\nНаказаний: 10 штук",
+        "ГС": "Ответы: 100 штук\nОнлайн: 150 минут\nНаказаний: 5 штук",
+        "К": "Ответы: 50 штук\nОнлайн: 150 минут"
+    }
+
+    if post == "Младший Модератор":
+        standard = str(rank_standards['ММ'])
+    elif second == "МФ":
+        standard = str(rank_standards['МФ'])
+    elif "МК" in second and "МФ" in second:
+        standard = str(rank_standards['МК&МФ'])
+    elif "МК" in second:
+        standard = str(rank_standards['МК'])
+    elif post == "Модератор":
+        standard = str(rank_standards['М'])
+    elif post == "Администратор ":
+        standard = str(rank_standards['А'])
+    elif post == "Старший Администратор":
+        standard = str(rank_standards['СА'])
+    elif "Заместитель Главного Следящего" in post:
+        standard = str(rank_standards['ЗГС'])
+    elif "Главный Следящий" in post:
+        standard = str(rank_standards['ГС'])
+    else:
+        standard = str(rank_standards['К'])
+
+    return f"Ваш ежедневный норматив:\n{standard}"
 
 
 vk_session = vk_api.VkApi(token=cfg.TOKEN)
@@ -116,19 +153,26 @@ while True:
 
                 if access(user_id)[1] == 1 and text == "Основная информация":
                     line_id = access(user_id)[0]
-                    row = sheet.row_values(line_id)
-                    values_array = list(row)
+                    values = sheet.row_values(line_id)
+                    values_array = list(values)
                     sender(user_id, get_default_info(values_array))
 
                 elif access(user_id)[1] == 1 and text == "Информация о повышениях":
                     line_id = access(user_id)[0]
-                    row = sheet.row_values(line_id)
-                    values_array = list(row)
+                    values = sheet.row_values(line_id)
+                    values_array = list(values)
                     sender(user_id, get_info_about_rank(values_array))
+
+                elif access(user_id)[1] == 1 and text == "Ежедневная норма":
+                    line_id = access(user_id)[0]
+                    values = sheet.row_values(line_id)
+                    values_array = list(values)
+                    sender(user_id, get_rank_standard([values_array[2], values_array[3]]))
 
                 elif event.text.lower() == "начать":
                     keyboard = VkKeyboard()
                     keyboard.add_button("Основная информация", VkKeyboardColor.POSITIVE)
+                    keyboard.add_button("Ежедневная норма", VkKeyboardColor.PRIMARY)
                     keyboard.add_line()
                     keyboard.add_button("Информация о повышениях", VkKeyboardColor.NEGATIVE)
                     vk_session.method("messages.send", {
@@ -137,7 +181,7 @@ while True:
                         "random_id": 0,
                         "keyboard": keyboard.get_empty_keyboard()
                     })
-                    time.sleep(1)
+                    time.sleep(0.25)
                     vk_session.method("messages.send", {
                         "user_id": user_id,
                         "message": "Кнопки успешно созданы!",
@@ -146,24 +190,25 @@ while True:
                     })
 
                 # для фиксов и переработки кнопок
-                elif event.text.lower() == "full_reset_buttons" and user_id in cfg.DEV:
+                elif event.text.lower() == "update_buttons" and user_id in cfg.DEV:
                     ids_column = sheet.col_values(7)
                     keyboard = VkKeyboard()
                     keyboard.add_button("Основная информация", VkKeyboardColor.POSITIVE)
+                    keyboard.add_button("Ежедневная норма", VkKeyboardColor.PRIMARY)
                     keyboard.add_line()
                     keyboard.add_button("Информация о повышениях", VkKeyboardColor.NEGATIVE)
                     for admin_id in ids_column:
                         try:
                             vk_session.method("messages.send", {
                                 "user_id": admin_id,
-                                "message": "Загрузка обновления...",
+                                "message": "Обновление кнопок...",
                                 "random_id": 0,
                                 "keyboard": keyboard.get_empty_keyboard()
                             })
-                            time.sleep(1)
+                            time.sleep(0.25)
                             vk_session.method("messages.send", {
                                 "user_id": admin_id,
-                                "message": "Кнопки были успелно обновлены\n\nЕсли с ними произошел баг, сообщите сюда: @xm_pearson\n\nБот временно не работает, исправится к вечеру",
+                                "message": "Обновление успешно завершено!",
                                 "random_id": 0,
                                 "keyboard": keyboard.get_keyboard()
                             })
@@ -172,4 +217,4 @@ while True:
                             pass
 
     except Exception as error:
-        chat_sender(1, error)
+        print(error)
